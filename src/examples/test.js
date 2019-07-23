5e97a914c65979d9eda438e7a5e7e61edde0670e
Andrew <jabbrass@zoho.com>
2019-07-23 09:02:14 -0700

    Add console log to test dynamic import

--- src/examples/test.js
+++ src/examples/test.js
@@ -1,10 +1,1 @@
-import React from "react";
-
 console.log("hello dynamic import");
-
-const StateComponent = props => {
-  const color = props.color;
-  return <h1 style={{ color }}>dynamic module example</h1>;
-};
-
-export default StateComponent;
